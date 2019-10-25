6991cabbbf5eac1940b3eed670821462ebe4ef37
thascapegoat <maxikki1@gmail.com>
2019-10-25 15:06:58 -0400

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+r = {'name':'john','fruit':'orange'}
+for i in r.keys():
+    print(i)
+    for m in i:
+        print(m)
+    for n in r.values():
+        print(n)
+        for b in n:
+            print(b)
